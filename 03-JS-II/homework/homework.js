6c91260996010be4f60db0085808d3d7f2f338a9
raulcontrerasmiranda <71307761+raulcontrerasmiranda@users.noreply.github.com>
2022-07-13 20:55:27 -0700

    tarea js II realizada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,31 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-}
+  var mayor = null;
+  if(x===y || x > y){
+    mayor = x;
+  } else {
+    mayor = y;
+    }
+
+    return mayor;
+
+  }
+  
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+
+  if(edad >= 18){
+    return "Allowed";
+  } else {
+    return "Not Allowed";
+  }
+
+
+
 }
   
 function conection(status) {
@@ -19,6 +38,18 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+
+if(status === 1){
+  return "Online";
+} else {
+  if(status === 2){
+    return "Away";
+  } else {
+    return "Offline";
+  }
+}
+
+
 }
 
 function saludo(idioma) {
@@ -28,6 +59,21 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+
+  switch(idioma) {
+    case "aleman":
+      alert("Guten Tag!")
+      break;
+    case "mandarin":
+      alert("Ni Hao!")
+      break;
+    case "ingles":
+      alert("Hello!")
+      break;
+    default:
+      alert("Hola!")
+
+  }
 }
 
 function colors(color) {
@@ -38,18 +84,50 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+
+   var msg = null;
+  switch(color){
+    case "blue":
+      msg = "This is blue";
+      break;
+    case "red":
+      msg = "This is red";
+      break;
+    case "green":
+      msg = "This is green";
+      break;
+    case "orange":
+      msg = "This is orange";
+      break;
+    default:
+      msg = "Color not found";
+
+  }
+  
+return msg;
+
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero === 10 || numero === 5){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero < 50 && numero > 20){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function esEntero(numero) {
@@ -60,6 +138,14 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if(numero % 1 === 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+
+
 }
 
 function fizzBuzz(numero) {
@@ -67,6 +153,23 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+
+  if(numero % 3 == 0){
+    return "fizz"
+  }
+
+  else if (numero % 5 == 0){
+    return "buzz"
+  }
+
+  else if (numero % 3 == 0 && numero % 5 == 0){
+    return "fizzbuzz"
+  }
+
+  else {
+    return numero;
+  }
+  
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -76,6 +179,26 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+
+  if(num1 > num2 && num1 > num3 && num >0){
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num1 < 0 || num2 < 0 || num3 < 0){
+    return "Hay negativos";
+  }
+  else if(num3 > num1 && num3 > num2){
+    num3 =+;
+    return num3;
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error";
+  }
+  else {
+    return false;
+  }
+
+
+
 }
 
 function esPrimo(numero) {
@@ -84,6 +207,22 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
+  if(numero === 1 && numero === 0){
+    return false;
+  }
+
+  else if(numero > 1){
+    for(let i = 2; i < numero; i++){
+      if(number % i !== 0){
+        return true; 
+        break;
+      }
+      else {
+        return false;
+      }
+    }
+  }
 }
 
 function esVerdadero(valor){
@@ -91,29 +230,56 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
+  if(valor == true){
+    return "Soy verdadero";
+  }
+  else { 
+    return "Soy falso"
+  }
+
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
+
+  var tablaDelSeis = [];
+  for (var i = 0; i == 10; i++){
+    tablaDelSeis[].push(i*6);
+
+  }
+  return tablaDelSeis[];
   
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  var digitos = numero.toString().length;
+  if(digitos === 3){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+
+  do {
+    numero += 5;
+  } while (numero < numero*8)
+
+  return numero;
+
 }
 
 
-// No modificar nada debajo de esta línea
+// No modificar nada debajo de esta línea 
 // --------------------------------
 
 module.exports = {
