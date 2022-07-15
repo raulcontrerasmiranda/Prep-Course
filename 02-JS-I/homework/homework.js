1b6b0099739ef989115d579f6bf286df610a3386
raulcontrerasmiranda <71307761+raulcontrerasmiranda@users.noreply.github.com>
2022-07-14 17:55:44 -0700

    correccion de un error detectado en el test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Hola";
 const nuevoNum = 9;
 
 // Crea una variable booleana:
-const nuevoBool = True;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
